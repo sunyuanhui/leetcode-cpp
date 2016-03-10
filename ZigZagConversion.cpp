50ce51d12a55b69b0dda280fa7b77ed3cefff361
sunyuanhui <ryan.sunyuanhui@gmail.com>
2016-03-10 23:45:38 +0800

    update some thought.

--- ZigZagConversion.cpp
+++ ZigZagConversion.cpp
@@ -12,53 +12,56 @@ public:
             return s;
         }
 
-        int tb = 2; // top & bottom
-        size_t s_len = s.length();
         string result;
-
-        for (int i = 0; i < numRows; ++i) {
-            if (i == numRows - 1) {
-                tb = 1;
-            }
-
-            int col = 0;
-            do 
-            {
-                int index = (tb + (numRows - tb) * 2) * col;
-                if (index < s_len) {
-                    result.push_back(s.at(index));
-                } else {
-                    break;
-                }
-                col++;
-            } while (1);
-        }
+        int column = calColumn(s, numRows);
         return result;
+    }
+    int calColumn(const string &s, int numRows) {
+        size_t len = s.length();
+        int column = 0;
+        const int tb = 2; // top & bottom
+        const int column_num = numRows + (numRows - tb);
+        while (len != 0) {
+            column++;
+            if (len < column_num) {
+                break;
+            }
+            len = len - column_num;
+        }
+        return column;
     }
 };
 
 TEST_F(LeetCodeTestFixture, ZigZagConversion) {
     Solution solution;
+    string origin = "PAYPALISHIRING";
 	string result;
+    int column;
 
-	result = solution.convert("PAYPALISHIRING", 1);
+	result = solution.convert(origin, 1);
     EXPECT_EQ(result, "PAYPALISHIRING");
 
     //P Y A I H R N
     //A P L S I I G
-	result = solution.convert("PAYPALISHIRING", 2);
+    column = solution.calColumn(origin, 2);
+    EXPECT_EQ(column, 7);
+	result = solution.convert(origin, 2);
     EXPECT_EQ(result, "PYAIHRNAPLSIIG");
 
     //P   A   H   N
     //A P L S I I G
     //Y   I   R
-	result = solution.convert("PAYPALISHIRING", 3);
+    column = solution.calColumn(origin, 3);
+    EXPECT_EQ(column, 4);
+	result = solution.convert(origin, 3);
     EXPECT_EQ(result, "PAHNAPLSIIGYIR");
 
     //P     I     N
     //A   L S   I G
     //Y A   H R 
     //P     I 
-	result = solution.convert("PAYPALISHIRING", 4);
+    column = solution.calColumn(origin, 4);
+    EXPECT_EQ(column, 3);
+	result = solution.convert(origin, 4);
     EXPECT_EQ(result, "PINALSIGYAHRPI");
 }
