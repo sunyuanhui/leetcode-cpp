a04b847e3cd9df5a27647ccba9d37ee528419568
sunyuanhui <ryan.sunyuanhui@gmail.com>
2016-03-21 20:02:51 +0800

    Still not finish..

--- StringtoInteger.cpp
+++ StringtoInteger.cpp
@@ -6,14 +6,17 @@ using namespace std;
 class Solution {
 public:
     int myAtoi(string str) {
-        const int error = 0;
-        if (str.size() == 0) {
-            return error;
+        size_t start = str.find_first_not_of(' ');
+        if (start == string::npos || str.size() == 0) {
+            return 0;
         }
 
-        size_t start = str.find_first_not_of(' ');
-        if (start == string::npos) {
-            return error;
+        int sign = 1;
+        if (str.at(start) == '+') {
+            start++;
+        } else if (str.at(start) == '-') {
+            start++;
+            sign = -1;
         }
 
         int integer = 0;
@@ -21,19 +24,18 @@ public:
             char num_char = str.at(i);
             if ('0' <= num_char && num_char <= '9') {
                 int number = num_char - '0';
-                int next = integer * 10 + number;
-                if (next - integer * 10 == number) {
-                    integer = next;
-                } else {
-                    return error;
+                integer = integer * 10 + number;
+                if (sign > 0 && integer <= 0) {
+                    return INT_MAX;
+                } else if (sign < 0 && integer >= 0) {
+                    return INT_MIN;
                 }
-            } else if (num_char == ' ') {
-                continue;
             } else {
-                return error;
+                break;
             }
         }
-        return integer;
+
+        return integer * sign;
     }
 };
 
@@ -45,10 +47,22 @@ TEST_F(LeetCodeTestFixture, StringToInteger) {
 
 	integer = solution.myAtoi("  4321  ");
     EXPECT_EQ(integer, 4321);
+    
+	integer = solution.myAtoi("  43a21  ");
+    EXPECT_EQ(integer, 43);
 
 	integer = solution.myAtoi("  4321 1 ");
-    EXPECT_EQ(integer, 43211);
+    EXPECT_EQ(integer, 4321);
 
 	integer = solution.myAtoi("  +1 ");
     EXPECT_EQ(integer, 1);
+
+	integer = solution.myAtoi("  -1 ");
+    EXPECT_EQ(integer, -1);
+
+	integer = solution.myAtoi("2147483648");
+    EXPECT_EQ(integer, 2147483647);
+
+	integer = solution.myAtoi("9223372036854775809");
+    EXPECT_EQ(integer, 2147483647);
 }
